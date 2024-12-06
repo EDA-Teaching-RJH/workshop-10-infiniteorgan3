096ec80450566ea599f763a0fb48af5a9fd66175
Ellie Marsden <ejam2@kent.ac.uk>
2024-12-06 15:39:49 +0000

    Added code to the classes file

---
+++ Classes.py
@@ -0,0 +1,66 @@
+class Student:
+    def __init__(self, name, degree, studentid):
+        if not name:
+            raise ValueError("The name is invalid because it is empty.")
+        if degree not in ["ECE", "BIO", "MECH", "EEE"]:
+            raise ValueError("Invalid degree input.")
+        if len(str(studentid)) != 6 or type(studentid) is not int:
+            raise ValueError("The student ID entered is invalid.")
+        self.name = name
+        self.degree = degree
+        self.studentid = studentid
+    
+    def __str__(self):
+        return f"The student {self.studentid} is {self.name} from {self.degree}."
+
+    @property
+    def degree(self):
+        return self._degree
+    
+    @degree.setter
+    def degree(self, degree):
+        if degree not in ["ECE", "BIO", "MECH", "EEE"]:
+            raise ValueError("Invalid degree input.")
+        self.degree = degree
+
+class Course:
+    def __init__(self, coursename, coursecode, enrolledstudents):
+        if not coursename:
+            raise ValueError("The name of the course is invalid because it is empty.")
+        if not coursecode or type(coursecode) is not str:
+            raise ValueError("The course code is invalid.")
+        if not enrolledstudents or type(enrolledstudents) is not list:
+            raise ValueError("The ")
+        self.coursename = coursename
+        self.coursecode = coursecode
+        self.enrolledstudents = enrolledstudents
+    
+    def addstudent(self, student):
+        self.enrolledstudents.append(student)
+    
+    def removestudent(self, student):
+        self.enrolledstudents.remove(student)
+    
+    def printallstudents(self):
+        print("The students in this course are: ")
+        for i in self.enrolledstudents:
+            print(i)
+
+class GraduateStudent(Student):
+    def __init__(self, name, degree, studentid, researchtopic):
+        super().__init__(name, degree, studentid)
+        if not researchtopic or type(researchtopic) is not str:
+            raise ValueError("The research topic is invalid.")
+        self.researchtopic = researchtopic
+    
+class UndergraduateStudent(Student):
+    def __init__(self, name, degree, studentid, yearinindustry, foundation, repeater):
+        super().__init__(name, degree, studentid)
+        if not yearinindustry or type(yearinindustry) is not bool:
+            raise ValueError("The value for whether the student is doing a degree in industry is invalid.")
+        self.yearinindustry = yearinindustry
+        if not foundation or type(foundation) is not bool:
+            raise ValueError("The value for whether the student has done a foundation year is invalid.")
+        self.foundation = foundation
+        if not repeater or type(repeater) is not bool:
+            raise ValueError("The value for whether the student is repeating the year is invalid.")
