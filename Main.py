e19d541c3900ce1b901d74981083b05045fa42c9
Ellie Marsden <ejam2@kent.ac.uk>
2024-12-06 16:19:33 +0000

    a

--- Main.py
+++ Main.py
@@ -6,11 +6,18 @@ def main():
     student3 = Classes.GraduateStudent("C", "BIO", 111111, "a")
     student4 = Classes.UndergraduateStudent("D", "EEE", 222222, True, True, False)
     coursea = Classes.Course("a", "ssssss", list())
-
+    courseb = Classes.Course("b", "a", list())
     coursea.addstudent(student1)
     coursea.addstudent(student4)
     coursea.addstudent(student3)
-    
+
+    courseb.addstudent(student1)
+    courseb.addstudent(student2)
+
+    coursec = coursea + courseb
+
+    print(coursec)
+
     print(coursea)
 
     print(student1.degree)
