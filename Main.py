cfa64631bc7acfe73928686f27310e90340e3a02
Ellie Marsden <ejam2@kent.ac.uk>
2024-12-06 15:54:54 +0000

    Added more code

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
 import Classes
+
+def main():
+    student1 = Classes.UndergraduateStudent("A", "ECE", 999999, False, True, False)
+    student2 = Classes.GraduateStudent("B", "MECH", 123456, "ffffff")
+    student3 = Classes.GraduateStudent("C", "BIO", 111111, "a")
+    student4 = Classes.UndergraduateStudent("D", "EEE", 222222, True, True, False)
+    coursea = Classes.Course("a", "ssssss", [])
+
+    coursea.addstudent(student1)
+    coursea.addstudent(student4)
+
+    print(coursea)
+
+    print(student1.degree())
+
+    student2.degree("ECE")
